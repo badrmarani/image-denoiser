3cd52a970a2d3a266071a1e8597aa573a1c9fe7e
badrmarani <badr-eddine.marani@centrale.centralelille.fr>
2022-09-29 18:08:36 +0200

    delete unnecessary files

--- source/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .noise import gaussian
-
-from .operators import div
-from .operators import grad
-from .operators import hessian
-from .operators import hessian_adjoint
--- source/utils/noise.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-# third party
-import numpy as np
-
-def gaussian (im, mean = 0, sigma = 20) :
-    return im + np.random.normal (mean, sigma, np.shape (im))
